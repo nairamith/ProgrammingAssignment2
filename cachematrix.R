ea403df2968e21b3595b2157cfddd7836d1b7517
Amith Nair <amithnair2@gmail.com>
2020-08-16 18:21:25 +0530

    Function to calculate the inverse of a matrix using cachinng mechanism

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below can be used to calculate the  inverse of a 
+## matrix. The functions use caching mechanism to avoid the 
+## costly computation involved with getting the inverse of a matrix
+## repeatedly
 
-## Write a short comment describing this function
+## The function takes a matrix input and returns a 
+## matrix of funcions to
+## 1. Set the matrix
+## 2. Set and cache the inverse of the matrix
+## 3. Get the matrix
+## 4. Get the inverse of the matrix if available in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  i<-NULL
+  set<-function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get<-function()  x
+  
+  setInverse<-function(inverse) {
+    i<<-inverse
+  }
+  getInverse<-function() i
+  
+  matrix(c(set, setInverse, 
+           get, getInverse), 2,2, byrow = TRUE,
+         dimnames = list(c("set", "get"),c("Matrix","Inverse")) )
 
 }
 
 
-## Write a short comment describing this function
+## This method takes the return value of the function
+## makCacheMatrix and returns the inverse of the matrixbound to
+## the passed function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse<-x["get", "Inverse"][[1]]()
+  if(!is.null(inverse)){
+    message("Reading Fromm Cache")
+    return(inverse)
+  }
+  matrix<-x["get", "Matrix"][[1]]()
+  inverse<-solve(matrix)
+  x["set", "Inverse"][[1]](inverse)
+  inverse
 }
